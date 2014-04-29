d08097d8cd9e177eb9fa9f7b6731880c4d415c61
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-04-29 21:40:44 +0100

    Update 4_PermCheck.java

--- 4_PermCheck.java
+++ 4_PermCheck.java
@@ -5,9 +5,15 @@ class Solution {
         int sum = 0;
         int result;
         long n = A.length*(A.length+1)/2;
+        int[] B = new int[A.length + 1];
         
         for(int i = 0; i < A.length; i++) {
+            B[A[i]-1]++;
             sum += A[i];
+        }
+        
+        for(int i = 0; i < B.length; i++) {
+            if (B[i] > 1) return 0;
         }
         
         if(n - sum == 0) { 
