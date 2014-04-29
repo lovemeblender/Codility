8482cdc0e812213eeef569b613a6714ba1b4a5de
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-04-29 20:39:07 +0100

    Update 3_PermMissingElem.java

--- 3_PermMissingElem.java
+++ 3_PermMissingElem.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int[] A) {
         
         int sum = 0;
-        int n = A.length + 1;
+        long n = A.length + 1;
         
         for(int i = 0; i < A.length; i++) {
             sum += A[i];
         }
-        
-        return ((n*(n + 1)) / 2) - sum;
+        int result = (int) ((n*(n + 1)) / 2) - sum;
+        return result;
     }
 }
