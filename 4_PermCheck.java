49a43f919bb07511fe33af175590ef1b14865831
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-04-30 01:44:58 +0100

    Update 4_PermCheck.java

--- 4_PermCheck.java
+++ 4_PermCheck.java
@@ -1,26 +1,17 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int[] A) {
         
-        int sum = 0;
-        int result;
-        long n = A.length*(A.length+1)/2;
-        ArrayList<Integer> B = new ArrayList<Integer>();
+        int[] B = new int[A.length];
         
         for(int i = 0; i < A.length; i++) {
-            if( B.contains(A[i]-1) ) return 0;
-            B.add( A[i]-1 );
-            sum += A[i];
+            if(A[i] > A.length) return 0;
+            B[ A[i] - 1 ]++;
         }
         
-        if(n - sum == 0) { 
-            result = 1;
-        }
-        else {
-            result = 0;
+        for(int i = 0; i < B.length; i++) {
+            if (B[i] > 1) return 0;
         }
         
-        return result;
+        return 1;
     }
 }
