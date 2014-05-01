562d187c2cdfece700ce2c77541bc490c0197c8e
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-05-01 01:43:57 +0100

    Update 2_MaxCounters.java

--- 2_MaxCounters.java
+++ 2_MaxCounters.java
@@ -1,24 +1,32 @@
 class Solution {
     public int[] solution(int N, int[] A) {
         int[] B = new int[N];
-        int max = 0;
+        int lMax = 0, max = 0;
         
         for(int i = 0; i < A.length; i++) {
             
             if(A[i] > N) {
-                // set all to max
-                for(int j = 0; j < N; j++) {
-                    B[j] = max;
-                }
+                lMax = max;
             }
             else {
+                if(B[A[i] - 1] < lMax) {
+                    B[A[i] - 1] = lMax + 1;
+                }
+                
                 int newMax = ++B[A[i] - 1];
                 if(newMax > max) {
                     max = newMax;
                 }
+                
             }
-            
         }
+        
+        for(int j = 0; j < N; j++) {
+            if(B[j] < lMax) {
+                B[j] = lMax;
+            }
+        }
+        
         return B;
     }
 }
