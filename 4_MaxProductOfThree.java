60d46b38a5a9cc41467f13311464032ef78f7158
Michael Despotopoulos <michaeldespoto@gmail.com>
2014-05-08 02:53:19 +0100

    Update 4_MaxProductOfThree.java

--- 4_MaxProductOfThree.java
+++ 4_MaxProductOfThree.java
@@ -5,10 +5,7 @@ class Solution {
         
         Arrays.sort(A);
         int N = A.length - 1;
-        
-        int a = A[0] * A[1] * A[N];
-        int b = A[N - 2] * A[N - 1] * A[N];
-        
-        return Math.max(a, b);
+
+        return Math.max(A[0] * A[1] * A[N], A[N - 2] * A[N - 1] * A[N]);
     }
 }
